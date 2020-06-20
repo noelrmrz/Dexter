dde13b7221ffe934d72722a6e5fd83ea9ba0b440
noelrmrz <noel.rmrz@gmail.com>
2020-06-20 16:05:53 +0200

    Added classes to implement custom RecyclerViewItemDecoration

---
+++ app/src/main/java/com/noelrmrz/pokedex/ui/DividerItemDecoration.java
@@ -0,0 +1,45 @@
+package com.noelrmrz.pokedex.ui;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+public class DividerItemDecoration extends RecyclerView.ItemDecoration {
+    private Drawable mDivider;
+
+    public DividerItemDecoration(Drawable divider) {
+        mDivider = divider;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+
+        // For the first item do not draw an offset above the RecyclerView
+        if (parent.getChildAdapterPosition(view) == 0) {
+            return;
+        }
+
+        outRect.top = mDivider.getIntrinsicHeight();
+    }
+
+    @Override
+    public void onDraw(Canvas canvas, RecyclerView parent, RecyclerView.State state) {
+        // Calculate left and right bounds of RecyclerView
+        int dividerLeft = parent.getPaddingLeft();
+        int dividerRight = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        // Calculate the top and bottom of each divider for each child view
+        for (int i = 0; i < childCount - 1; i++) {
+            View child = parent.getChildAt(i);
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+            int dividerTop = child.getBottom() + params.bottomMargin;
+            int dividerBottom = dividerTop + mDivider.getIntrinsicHeight();
+
+            mDivider.setBounds(dividerLeft, dividerTop, dividerRight, dividerBottom);
+            mDivider.draw(canvas);
+        }
+    }
+}
